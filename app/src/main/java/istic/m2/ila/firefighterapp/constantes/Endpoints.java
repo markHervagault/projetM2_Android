21f3b6b0b71b4cd8b184f2c77be3975c5a792b6f
toto <hkebaili92@gmail.com>
2018-03-21 11:18:09 +0100

    classe de constantes Endpoint

---
+++ app/src/main/java/istic/m2/ila/firefighterapp/constantes/Endpoints.java
@@ -0,0 +1,12 @@
+package istic.m2.ila.firefighterapp.constantes;
+
+/**
+ * Created by hakima on 3/21/18.
+ */
+
+public class Endpoints {
+    public static final String BASE = "loaclahost:8080/api";
+    public static final String INTERVENTION_ID = "";
+    public static final String VEHICULE = "";
+    public static final String AUTHENTICATE = "authenticate";
+}
