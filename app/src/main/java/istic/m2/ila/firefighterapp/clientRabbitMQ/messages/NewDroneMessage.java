f96d27fa7b6e6e2df2da1f4fa72443fc9e9bbcd3
mark <mark.hervagault@gmail.com>
2018-03-28 12:48:04 +0200

    MAJ rabbitMQ

--- app/src/main/java/istic/m2/ila/firefighterapp/clientRabbitMQ/messages/NewDroneMessage.java
+++ app/src/main/java/istic/m2/ila/firefighterapp/clientRabbitMQ/messages/NewDroneMessage.java
@@ -4,5 +4,25 @@ package istic.m2.ila.firefighterapp.clientRabbitMQ.messages;
  * Created by markh on 28/03/2018.
  */
 
-public class NewDroneMessage {
+public class NewDroneMessage implements MessageBus{
+
+    private long droneId;
+
+    /**
+     * Constructeur
+     * @param droneId
+     *      id du nouveau drone
+     */
+    public NewDroneMessage(long droneId) {
+        this.droneId = droneId;
+    }
+
+    public long getDroneId() {
+        return droneId;
+    }
+
+    public void setDroneId(long droneId) {
+        this.droneId = droneId;
+    }
+
 }
