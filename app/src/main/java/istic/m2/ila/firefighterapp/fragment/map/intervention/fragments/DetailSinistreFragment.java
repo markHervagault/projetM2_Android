612c508637d93060a594e4775cc04e0a6580f9f5
VDS <demeautis.victor@gmail.com>
2018-04-24 11:16:50 +0200

    detail SInistre

---
+++ app/src/main/java/istic/m2/ila/firefighterapp/fragment/map/intervention/fragments/DetailSinistreFragment.java
@@ -0,0 +1,55 @@
+package istic.m2.ila.firefighterapp.fragment.map.intervention.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import istic.m2.ila.firefighterapp.R;
+import istic.m2.ila.firefighterapp.dto.SinistreDTO;
+import istic.m2.ila.firefighterapp.dto.TraitTopoDTO;
+
+public class DetailSinistreFragment extends Fragment {
+    private static final String ARG = "data";
+    private SinistreDTO sinistre;
+
+    public DetailSinistreFragment() {}
+
+    public static DetailSinistreFragment newInstance(SinistreDTO dto) {
+        DetailSinistreFragment fragment = new DetailSinistreFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG, dto);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            sinistre = (SinistreDTO) getArguments().getSerializable(ARG);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_detail_sinistre, container, false);
+        ((TextView)this.getActivity().findViewById(R.id.typeValue)).setText(sinistre.getType().toString());
+        ((TextView)this.getActivity().findViewById(R.id.composanteValue)).setText(sinistre.getComposante().getDescription());
+        this.getActivity().findViewById(R.id.supprimer).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteSinistre();
+            }
+        });
+        return view;
+    }
+
+    private void deleteSinistre(){};
+}
